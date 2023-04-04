80bcd1a4b558f3b594e73449743f58eae4044a12
MyDum-bsu <mydum.bsu@gmail.com>
2023-04-04 22:23:22 +0300

    Created Dao classes

---
+++ EduDocs.API/src/main/java/by/fpmibsu/edudocs/DAO/RequestDao.java
@@ -0,0 +1,38 @@
+package by.fpmibsu.edudocs.DAO;
+
+import by.fpmibsu.edudocs.entities.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public class RequestDao<T extends User> extends AbstractDao<T>  {
+    @Override
+    public List<T> findAll() {
+        return null;
+    }
+
+    @Override
+    public T findEntityById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(UUID id) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(T entity) {
+        return false;
+    }
+
+    @Override
+    public boolean create(T entity) {
+        return false;
+    }
+
+    @Override
+    public T update(T entity) {
+        return null;
+    }
+}
