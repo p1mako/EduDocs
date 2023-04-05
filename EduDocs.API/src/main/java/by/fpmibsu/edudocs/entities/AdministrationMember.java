4740b544a1d302abf2491ab66b6065461382561a
Dmitry Leshchik <fpm.leschik@bsu.by>
2023-04-05 14:52:05 +0300

    Remove dependency to database structure

--- EduDocs.API/src/main/java/by/fpmibsu/edudocs/entities/AdministrationMember.java
+++ EduDocs.API/src/main/java/by/fpmibsu/edudocs/entities/AdministrationMember.java
@@ -5,20 +5,26 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public class AdministrationMember extends User {
 
-    public AdministrationMember(@NotNull AdministrationRole role, @NotNull Timestamp from, @Nullable Timestamp until,
-                                @NotNull String login, @NotNull String password, @NotNull String name,
-                                @NotNull String surname, @Nullable String lastName, @Nullable UUID id) {
+    AdministrationRole role;
+    Timestamp from;
+    Timestamp until;
+    List<Template> availableTemplates;
+
+    public AdministrationMember(@Nullable UUID id, @NotNull AdministrationRole role, @NotNull Timestamp from,
+                                @Nullable Timestamp until, @NotNull String login, @NotNull String password,
+                                @NotNull String name, @NotNull String surname, @Nullable String lastName,
+                                @Nullable List<Template> availableTemplates) {
         super(login, password, name, surname, lastName, id);
         this.role = role;
         this.from = from;
         this.until = until;
+        this.availableTemplates = availableTemplates;
     }
-
-    AdministrationRole role;
-    Timestamp from;
-    Timestamp until;
 }
