f85572c1bd53079e1d36e3d9d91ec18b469615dd
Dmitry Leshchik <fpm.leschik@bsu.by>
2023-05-19 14:29:26 +0300

    added template and fixed some bugs and errors

--- EduDocsAPI/src/main/java/by/fpmibsu/edudocs/servlets/GetAllRequests.java
+++ EduDocsAPI/src/main/java/by/fpmibsu/edudocs/servlets/GetAllRequests.java
@@ -6,14 +6,21 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "GetAllRequests", value = "/GetAllRequests")
+@WebServlet(name = "GetAllRequests", value = "/get-all-requests")
 public class GetAllRequests extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
+
+        if (request.getSession(false) == null){
+            response.setStatus(403);
+            return;
+        }
+
         try {
-            writer.println("<h2>Hello from HelloServlet</h2>");
+            writer.println("<h2>" + request.getParameter("user") + "</h2>");
+            writer.println("<h2>" + request.getParameter("password") + "</h2>");
         } finally {
             writer.close();
         }
@@ -21,6 +28,5 @@ public class GetAllRequests extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
