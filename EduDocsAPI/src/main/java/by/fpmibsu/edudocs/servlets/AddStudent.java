e474a1f31ee2c508173ec934f12d4f2efae79283
Stanislav <89999222+StanislavKhartov@users.noreply.github.com>
2023-05-26 19:33:11 +0300

    add add-student servlet

--- EduDocsAPI/src/main/java/by/fpmibsu/edudocs/servlets/AddStudent.java
+++ EduDocsAPI/src/main/java/by/fpmibsu/edudocs/servlets/AddStudent.java
@@ -2,8 +2,13 @@ package by.fpmibsu.edudocs.servlets;
 
 import by.fpmibsu.edudocs.App;
 import by.fpmibsu.edudocs.dao.DaoException;
+import by.fpmibsu.edudocs.dao.SpecializationDao;
+import by.fpmibsu.edudocs.dao.StudentDao;
 import by.fpmibsu.edudocs.dao.UserDao;
+import by.fpmibsu.edudocs.entities.Student;
+import by.fpmibsu.edudocs.entities.Template;
 import by.fpmibsu.edudocs.entities.User;
+import by.fpmibsu.edudocs.entities.utils.StudentStatus;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -13,15 +18,19 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Properties;
+import java.util.StringTokenizer;
+import java.util.UUID;
 
 @WebServlet(name = "AddUser", value = "/add-student")
-public class Login extends HttpServlet {
+public class AddStudent extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        UserDao userDao = new StudentDao();
+        StudentDao userDao = new StudentDao();
+        SpecializationDao sd = new SpecializationDao();
 
         try {
             DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
@@ -41,6 +50,7 @@ public class Login extends HttpServlet {
         try {
             Connection con = DriverManager.getConnection(url, prop);
             userDao.setConnection(con);
+            sd.setConnection(con);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -52,24 +62,25 @@ public class Login extends HttpServlet {
                 request.getParameter("entryDate") == null ||
                 request.getParameter("group") == null ||
                 request.getParameter("status") == null ||
-                request.getParameter("uniqueNumber") == null,
+                request.getParameter("uniqueNumber") == null ||
                 request.getParameter("specialization") == null){
             response.setStatus(422);
             return;
         }
-        try {
-            user = new Student(request.getParameter("login"),
+       try {
+            User user = new Student(null,
+                    request.getParameter("login"),
                     request.getParameter("password"),
                     request.getParameter("name"),
                     request.getParameter("surname"),
                     request.getParameter("lastname"),
                     Timestamp.valueOf(request.getParameter("entryDate")),
                     Integer.valueOf(request.getParameter("group")),
-                    StudentStatus.valueOf(Integer.valueOfrequest.getParameter("status")),
                     Integer.valueOf(request.getParameter("uniqueNumber")),
-                    Specialization.valueOf(Integer.valueOf(request.getParameter("specialization"))));
-            if(userDao.create(user)){
-                writer.println("Success!");
+                    StudentStatus.valueOf(request.getParameter("status")),
+                    sd.findEntityById(UUID.fromString((request.getParameter("specialization")))));
+            if(userDao.create((Student) user)){
+
             }
         } catch (DaoException e) {
             response.setStatus(422);
