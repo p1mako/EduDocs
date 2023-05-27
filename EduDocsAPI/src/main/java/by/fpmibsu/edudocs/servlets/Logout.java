dc915e49ef6c10fcc6e0af38418c94b0e86b682e
Stanislav <89999222+StanislavKhartov@users.noreply.github.com>
2023-05-27 10:24:37 +0300

    add logout servlet

---
+++ EduDocsAPI/src/main/java/by/fpmibsu/edudocs/servlets/Logout.java
@@ -0,0 +1,24 @@
+package by.fpmibsu.edudocs.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Logout", value = "/logout")
+public class Logout extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        java.io.PrintWriter writer = response.getWriter();
+        writer.println("Success!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
