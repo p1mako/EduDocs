b28b7a475101328d2d553a113ac03f91b23501ad
Stanislav <89999222+StanislavKhartov@users.noreply.github.com>
2023-05-29 15:34:33 +0300

    replace request creation action add template validator

--- EduDocsAPI/src/main/java/by/fpmibsu/edudocs/action/requests/RequestCreateAction.java
+++ EduDocsAPI/src/main/java/by/fpmibsu/edudocs/action/requests/RequestCreateAction.java
@@ -1,21 +1,18 @@
-package by.fpmibsu.edudocs.action;
+package by.fpmibsu.edudocs.action.requests;
 
+import by.fpmibsu.edudocs.action.Action;
 import by.fpmibsu.edudocs.dao.DaoException;
 import by.fpmibsu.edudocs.dao.IncorrectFormDataException;
-import by.fpmibsu.edudocs.entities.Entity;
 import by.fpmibsu.edudocs.entities.Request;
 import by.fpmibsu.edudocs.service.interfaces.RequestService;
-import by.fpmibsu.edudocs.service.interfaces.UserService;
-import by.fpmibsu.edudocs.validator.RequestValidator;
 import by.fpmibsu.edudocs.validator.Validator;
 import by.fpmibsu.edudocs.validator.ValidatorFactory;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class RequestAction extends Action {
+public class RequestCreateAction extends Action {
     @Override
     public void exec(HttpServletRequest request, HttpServletResponse response) throws DaoException {
         Validator<Request> validator = ValidatorFactory.createValidator(Request.class);
