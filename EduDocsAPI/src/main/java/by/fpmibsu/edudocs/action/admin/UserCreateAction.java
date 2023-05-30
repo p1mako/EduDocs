a2d9641db6add0dc09f166d06b3a6d914667395e
Stanislav <89999222+StanislavKhartov@users.noreply.github.com>
2023-05-30 10:33:04 +0300

    fix actions? but user edit acton is not completed.

--- EduDocsAPI/src/main/java/by/fpmibsu/edudocs/action/admin/UserCreateAction.java
+++ EduDocsAPI/src/main/java/by/fpmibsu/edudocs/action/admin/UserCreateAction.java
@@ -2,25 +2,46 @@ package by.fpmibsu.edudocs.action.admin;
 
 import by.fpmibsu.edudocs.dao.DaoException;
 import by.fpmibsu.edudocs.dao.IncorrectFormDataException;
+import by.fpmibsu.edudocs.entities.AdministrationMember;
 import by.fpmibsu.edudocs.entities.User;
 import by.fpmibsu.edudocs.service.interfaces.UserService;
 import by.fpmibsu.edudocs.validator.Validator;
 import by.fpmibsu.edudocs.validator.ValidatorFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class UserCreateAction extends AbstractAdministratorAction {
+
+    private static final Logger logger = LogManager.getLogger(UserListAction.class);
     @Override
     public void exec(HttpServletRequest request, HttpServletResponse response) throws DaoException {
+        if (request.getSession(false) == null) {
+            logger.error("Session is null");
+            response.setStatus(401);
+        } else {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                mapper.readValue(request.getSession().getAttribute("user").toString(), AdministrationMember.class);
+            } catch (JsonProcessingException e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                logger.info("Cannot respond user with user creation, because user is not an admin");
+            }
+        }
         UserService service = factory.getService(UserService.class);
         Validator<User> validator = ValidatorFactory.createValidator(User.class);
         try {
             User user = validator.validate(request);
             service.addUser(user);
+            response.setStatus(200);
         } catch (IncorrectFormDataException | IOException e) {
-            throw new RuntimeException(e);
+            logger.error("Error responding front with user creation");
+            response.setStatus(500);
         }
     }
 }
--- EduDocsAPI/src/main/java/by/fpmibsu/edudocs/action/admin/UserEditAction.java
+++ EduDocsAPI/src/main/java/by/fpmibsu/edudocs/action/admin/UserEditAction.java
@@ -1,17 +1,36 @@
 package by.fpmibsu.edudocs.action.admin;
 
 import by.fpmibsu.edudocs.dao.DaoException;
+import by.fpmibsu.edudocs.entities.AdministrationMember;
 import by.fpmibsu.edudocs.entities.User;
 import by.fpmibsu.edudocs.entities.utils.Role;
 import by.fpmibsu.edudocs.service.interfaces.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 public class UserEditAction extends AbstractAdministratorAction {
+
+    private static final Logger logger = LogManager.getLogger(UserListAction.class);
     @Override
     public void exec(HttpServletRequest request, HttpServletResponse response) throws DaoException {
+        if (request.getSession(false) == null) {
+            logger.error("Session is null");
+            response.setStatus(401);
+        } else {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                mapper.readValue(request.getSession().getAttribute("user").toString(), AdministrationMember.class);
+            } catch (JsonProcessingException e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                logger.info("Cannot respond user with user editon, because user is not an admin");
+            }
+        }
         request.setAttribute("roles", Role.values());
         UUID identity = UUID.fromString((String) request.getAttribute("identity"));
         UserService service = factory.getService(UserService.class);
