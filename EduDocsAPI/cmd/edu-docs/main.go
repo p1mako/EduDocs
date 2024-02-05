0f055818fd297fb04c7fe92d9d9cd02640e21f8f
Dmitry Leshchik <fpm.leschik@bsu.by>
2024-02-05 15:02:16 +0300

    set up swagger ui

--- EduDocsAPI/cmd/edu-docs/main.go
+++ EduDocsAPI/cmd/edu-docs/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "EduDocsAPI/internal/app"
+import (
+	"EduDocsAPI/internal/app"
+	"net/http"
+)
+
+var swaggerHome = "/home/skalem/IdeaProjects/EduDocs/EduDocsAPI/web/dist"
+var openapiHome = "/home/skalem/IdeaProjects/EduDocs/EduDocsAPI/api/"
+var swaggerAddressPrefix = "/swaggerui/"
+var openapiAddressPrefix = "/openapi/"
+
+func initSwagger() {
+	fs := http.FileServer(http.Dir(swaggerHome))
+	swaggerFS := http.FileServer(http.Dir(openapiHome))
+	http.Handle(swaggerAddressPrefix, http.StripPrefix(swaggerAddressPrefix, fs))
+	http.Handle(openapiAddressPrefix, http.StripPrefix(openapiAddressPrefix, swaggerFS))
+}
 
 func main() {
+	initSwagger()
 	app.Run()
 }
--- EduDocsAPI/internal/app/edu-docs.go
+++ EduDocsAPI/internal/app/edu-docs.go
@@ -1,7 +1,11 @@
 package app
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func Run() {
 	log.Default().Print("Starting server...")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
