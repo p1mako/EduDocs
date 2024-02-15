e2ef77cada19e9b5f1670e6aac24e45a1d3e877c
Dmitry Leshchik <fpm.leschik@bsu.by>
2024-02-16 00:59:10 +0300

    changed template reading from db

--- EduDocsAPI/internal/database/template.go
+++ EduDocsAPI/internal/database/template.go
@@ -16,7 +16,7 @@ func GetTemplateById(id uuid.UUID) (models.Template, error) {
 	if !query.Next() {
 		logger.InfoLog.Printf("No data was extracted for %s", id)
 	}
-	err = query.Scan(&template.Uuid, &template.RouteToDocument, &template.Name)
+	err = query.Scan(&template.Uuid, &template.RouteToDocument, &template.Name, &template.ResponsibleAdmin)
 	if err != nil {
 		logger.ErrorLog.Print("Could not read input from query: ", err)
 		return models.Template{}, err
@@ -37,7 +37,7 @@ func GetAllTemplates() ([]models.Template, error) {
 	}
 	for query.Next() {
 		var template models.Template
-		err = query.Scan(&template.Uuid, &template.RouteToDocument, &template.Name)
+		err = query.Scan(&template.Uuid, &template.RouteToDocument, &template.Name, &template.ResponsibleAdmin)
 		if err != nil {
 			logger.ErrorLog.Print("Model did not match the one in database")
 			return templates, err
@@ -48,7 +48,7 @@ func GetAllTemplates() ([]models.Template, error) {
 }
 
 func AddTemplate(template models.Template) error {
-	_, err := db.Query("INSERT INTO templates(route_to_document, name)  VALUES($1, $2)", template.RouteToDocument, template.Name)
+	_, err := db.Query("INSERT INTO templates(route_to_document, name, responsible_admin)  VALUES($1, $2, $3)", template.RouteToDocument, template.Name, template.ResponsibleAdmin)
 	if err != nil {
 		logger.ErrorLog.Print("Cannot add new template: ", err)
 	}
