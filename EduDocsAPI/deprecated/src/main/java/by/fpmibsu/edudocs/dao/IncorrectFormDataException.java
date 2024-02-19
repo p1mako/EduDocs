d88bb50d9ef682adb9d74ba0d4f712a9db049e23
Dmitry Leshchik <fpm.leschik@bsu.by>
2024-02-19 13:26:43 +0300

    removed java backend

--- EduDocsAPI/deprecated/src/main/java/by/fpmibsu/edudocs/dao/IncorrectFormDataException.java
+++
@@ -1,7 +0,0 @@
-package by.fpmibsu.edudocs.dao;
-
-public class IncorrectFormDataException extends Exception {
-	public IncorrectFormDataException(String param, String value) {
-		super(String.format("Empty or incorrect \"%s\" parameter was found: %s", param, value));
-	}
-}
